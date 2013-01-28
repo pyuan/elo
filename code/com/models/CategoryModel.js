43893ee179a88887a4e218ebfeabd7306f4c5721
Paul Yuan <pyuana@gmail.com>
2013-01-28 00:27:34 -0600

    just committing  the default jquery/backbonejs/requirejs project

---
+++ code/com/models/CategoryModel.js
@@ -0,0 +1,20 @@
+// Category Model
+// ==============
+
+// Includes file dependencies
+define([
+		
+		"jquery", 
+		"backbone"
+		
+	], function($, Backbone) {
+
+	// The Model constructor
+	var Model = Backbone.Model.extend({
+
+	});
+
+	// Returns the Model class
+	return Model;
+
+}); 
